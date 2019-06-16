1e44526ba59c2fb318011976b887ec0dc512b07a
alikhalid <ali.kahn92@gmail.com>
2019-06-15 21:27:15 -0500

    adding logger

---
+++ viewer/logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import logging
+
+def init_logger():
+    logging.basiConfig(filename='__viewer_cache__/event.log', filemode='w', format='%(asctime)s %(message)s')
+
+def get_logger():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    return logger
