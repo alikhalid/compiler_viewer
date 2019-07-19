b0a02368b23a67fc1294d1652c132d0bf2c792c3
alikhalid <ali.kahn92@gmail.com>
2019-07-18 23:25:37 -0500

    mock args cleanup

--- viewer/tests/test_detail/mock_args.py
+++ viewer/tests/test_detail/mock_args.py
@@ -34,15 +34,8 @@ def get_mock_args_failing():
 def get_mock_args_passing_a_out():
     args = {}
 
-    args['mode'] = 'INTERACTIVE'
     args['asm'] = 'a.out'
-    args['project_dir'] = 'viewer/tests/test_data/psssing_a_out'
-    args['watch_dirs'] = [args['project_dir']]
-    args['build_dir'] = 'viewer/tests/test_data/psssing_a_out'
-    args['include_dir'] = None
-    args['objdump_flags'] = []
-    args['build_flags'] = ["-std=c++17", "-O3"]
-    args['disable_parsing'] = False
+    args['build_dir'] = 'viewer/tests/test_data/passing_a_out'
 
     return args
 
@@ -50,14 +43,7 @@ def get_mock_args_passing_a_out():
 def get_mock_args_failing_a_out():
     args = {}
 
-    args['mode'] = 'INTERACTIVE'
     args['asm'] = 'a.out'
-    args['project_dir'] = 'viewer/tests/test_data/failing_a_out'
-    args['watch_dirs'] = [args['project_dir']]
     args['build_dir'] = 'viewer/tests/test_data/failing_a_out'
-    args['include_dir'] = None
-    args['objdump_flags'] = []
-    args['build_flags'] = ["-std=c++17", "-O3"]
-    args['disable_parsing'] = False
 
     return args
